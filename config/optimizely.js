879d120d23779601ada4eabb039e59b6d4744031
lisa pourier <lisapourier@gmail.com>
2025-02-07 20:40:28 +0100

    initializing SDK and adding a flag rule

---
+++ config/optimizely.js
@@ -0,0 +1,6 @@
+// config/optimizely.js
+import { createInstance } from '@optimizely/optimizely-sdk';
+
+const optimizely = createInstance({
+  sdkKey: 'FJS2GxMeUoGtW3KK3egjN', // [X] Provide the sdkKey of your desired environment here. this instance will load in this datafile.
+});
