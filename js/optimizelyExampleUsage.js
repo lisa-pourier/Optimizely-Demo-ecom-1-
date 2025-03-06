1c603a6e95f2a8a5e440cc8e8dce6a8680a1e8f2
lisa pourier <lisapourier@gmail.com>
2025-03-06 13:20:10 +0100

    new project SDK

--- js/optimizelyExampleUsage.js
+++ js/optimizelyExampleUsage.js
@@ -4,7 +4,7 @@
 import { createInstance } from '@optimizely/optimizely-sdk';
 
 const optimizely = createInstance({
-  sdkKey: 'FJS2GxMeUoGtW3KK3egjN', // [X] Provide the sdkKey of your desired environment here. this instance will load in this datafile.
+  sdkKey: 'LQPiHeAUmPHaFWB3r2Lmc', // [X] Provide the sdkKey of your desired environment here. this instance will load in this datafile.
 });
 
 // create a flag/feature:
